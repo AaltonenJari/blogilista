bfaa87cc664e821d2cd4cc4731ece295d7be959e
AaltonenJari <jaritapani.aaltonen@gmail.com>
2025-12-17 16:16:46 +0200

    blogilista, step2

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app') // varsinainen Express-sovellus
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// ÄLÄ KOSKAAN TALLETA SALASANOJA GitHubiin!
-const password = process.argv[2]
-const mongoUrl = `mongodb+srv://jaritapaniaaltonen_db_user:${password}@ccluster0.efy2nbv.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
