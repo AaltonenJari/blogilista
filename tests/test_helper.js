1ff630c925f7d44fe36d8c271832498a2ba3e302
AaltonenJari <jaritapani.aaltonen@gmail.com>
2025-12-22 14:37:56 +0200

    4.15: blogilistan laajennus, step3

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogss = [
   {
@@ -28,6 +29,12 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+
 module.exports = {
-  initialBlogss, nonExistingId, blogsInDb
+  initialBlogss, nonExistingId, blogsInDb, usersInDb
 }
